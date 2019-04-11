cdcfc1f0104e36fca6bb4284de25f238bb0b2889
locndv <locndv@phuc.am>
2019-04-11 14:31:04 +0700

    fix(src/index): register useTextStream function

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import { register } from 'bara'
 import App from './App'
 import './index.css'
-import { useReactApp, useTouchableStream } from './lib'
+import { useReactApp, useTouchableStream, useTextStream } from './lib'
 
 import { welcomeTrigger } from './examples/features/welcome'
 
 const BaraApp = () => {
   useReactApp('bara-app', App)
   useTouchableStream()
+  useTextStream()
   welcomeTrigger()
 }
 
