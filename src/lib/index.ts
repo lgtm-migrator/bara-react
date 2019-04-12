fa0b5c3f7e511c9ceb36bbd74136085689553fdf
nampdn <nampdn@protonmail.com>
2019-04-12 16:59:11 +0700

    fix(bara-react): export BaraProvider for React app

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -26,3 +26,4 @@ export const useReactApp = (name: string, App: ComponentType, rootHTML: string =
 export * from './exports'
 export * from './functions'
 export * from './models'
+export * from './context'
